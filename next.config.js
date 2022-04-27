57fac810a7cb867d5de38c90c2f7ed295bdcd1ff
Endy <nguyenduong971999@gmail.com>
2022-04-27 17:09:55 +0700

    chroe: fix eslint

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const { i18n } = require('./next-i18next.config');
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 module.exports = withPWA({
-  reactStrictMode: true,
+  swcMinify: true,
+  reactStrictMode: false,
   pwa: {
     disable: process.env.NODE_ENV === 'development',
     dest: 'public',
